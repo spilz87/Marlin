747d67b33d18844348bcc2147bd76ca01b375747
Spilz <reilhacpierre@hotmail.com>
2019-04-27 08:35:49 +0200

    resolution du probleme de temperature au demarage en passant la valeur à 1000

--- Marlin/src/HAL/HAL_LPC1768/HAL.h
+++ Marlin/src/HAL/HAL_LPC1768/HAL.h
@@ -148,7 +148,8 @@ using FilteredADC = LPC176x::ADC<ADC_LOWPASS_K_VALUE, ADC_MEDIAN_FILTER_SIZE>;
 #define HAL_ADC_READY()        FilteredADC::finished_conversion()
 
 // A grace period for the ADC readings to stabilize before they start causing thermal protection errors.
-#define THERMAL_PROTECTION_GRACE_PERIOD 500
+// PHR
+#define THERMAL_PROTECTION_GRACE_PERIOD 1000
 
 // Parse a G-code word into a pin index
 int16_t PARSED_PIN_INDEX(const char code, const int16_t dval);
