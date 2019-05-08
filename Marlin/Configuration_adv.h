59e225472792fa9ac5d66d1cb1b64e7543f8d7ec
Spilz <reilhacpierre@hotmail.com>
2019-05-08 08:34:53 +0200

    augmente purge length

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1376,7 +1376,7 @@
   //#define ADVANCED_PAUSE_CONTINUOUS_PURGE       // Purge continuously up to the purge length until interrupted.
   #define ADVANCED_PAUSE_PURGE_FEEDRATE        3  // (mm/s) Extrude feedrate (after loading). Should be slower than load feedrate.
   // PHR
-  #define ADVANCED_PAUSE_PURGE_LENGTH          0 // (mm) Length to extrude after loading.
+  #define ADVANCED_PAUSE_PURGE_LENGTH          10 // (mm) Length to extrude after loading.
                                                   //   Set to 0 for manual extrusion.
                                                   //   Filament can be extruded repeatedly from the Filament Change menu
                                                   //   until extrusion is consistent, and to purge old filament.
